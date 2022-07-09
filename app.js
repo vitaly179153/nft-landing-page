027da3331df0ac5cf7ff6366bbf988b099c54114
VitalRapo <88846184+vitaly179153@users.noreply.github.com>
2022-07-09 22:43:20 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,228 @@
+ // METAMASK CONNECTION
+const TIMEOUT = 1000;
+const COLLECTION_NAME = 'CodeCats';
+let editions = [];
+let dots = 1;
+
+window.addEventListener('DOMContentLoaded', () => {
+  const onboarding = new MetaMaskOnboarding();
+  const onboardButton = document.getElementById('connectWallet');
+  let accounts;
+
+  const updateButton = async () => {
+    if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
+      onboardButton.innerText = 'Install MetaMask!';
+      onboardButton.onclick = () => {
+        onboardButton.innerText = 'Connecting...';
+        onboardButton.disabled = true;
+        onboarding.startOnboarding();
+      };
+    } else if (accounts && accounts.length > 0) {
+      onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+      onboardButton.disabled = true;
+      onboarding.stopOnboarding();
+      checkOwner(accounts[0]);
+    } else {
+      onboardButton.innerText = 'Connect MetaMask!';
+      onboardButton.onclick = async () => {
+        await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        })
+        .then(function(accounts) {
+          onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+          onboardButton.disabled = true;
+          checkOwner(accounts[0]);
+        });
+      };
+    }
+  };
+
+  updateButton();
+  if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+    window.ethereum.on('accountsChanged', (newAccounts) => {
+      accounts = newAccounts;
+      updateButton();
+    });
+  }
+});
+
+const checkOwner = async (account) => {
+  if(account) {
+    let isOwner = false;
+    let page = 1
+    
+    const data = await fetchWithRetry(`/.netlify/functions/isowner/?wallet=${account}&page=${page}`);
+
+    isOwner = !isOwner ? data.isOwner : isOwner;
+    updateStatusText(isOwner, true)
+    
+    editions = [...data.editions]
+    let nextPage = data.next_page
+
+    while(nextPage) {
+      page = nextPage
+      const data = await fetchWithRetry(`/.netlify/functions/isowner/?wallet=${account}&page=${page}`);
+
+      isOwner = !isOwner ? data.isOwner : isOwner;
+      updateStatusText(isOwner, true)
+      
+      editions = [...editions, ...data.editions]
+      nextPage = data.next_page
+    }
+
+    updateStatusText(isOwner, false)
+  }
+}
+
+function updateStatusText(isOwner, checking) {
+  const statusText = document.querySelector('.owner-status');
+  if(checking) {
+    if(isOwner) {
+      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
+    } else {
+      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
+    }
+  } else {
+    if(isOwner) {
+      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
+    } else {
+      statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
+    }
+  }
+  dots = dots === 3 ? 1 : dots + 1;
+}
+
+function renderDots(dots) {
+  let dotsString = '';
+  for (let i = 0; i < dots; i++) {
+    dotsString += '.';
+  }
+  return dotsString;
+}
+
+function timer(ms) {
+  return new Promise(res => setTimeout(res, ms));
+}
+
+async function fetchWithRetry(url)  {
+  await timer(TIMEOUT);
+  return new Promise((resolve, reject) => {
+    const fetch_retry = (_url) => {
+      return fetch(_url).then(async (res) => {
+        const status = res.status;
+
+        if(status === 200) {
+          return resolve(res.json());
+        }            
+        else {
+          console.error(`ERROR STATUS: ${status}`)
+          console.log('Retrying')
+          await timer(TIMEOUT)
+          fetch_retry(_url)
+        }            
+      })
+      .catch(async (error) => {  
+        console.error(`CATCH ERROR: ${error}`)  
+        console.log('Retrying')    
+        await timer(TIMEOUT)    
+        fetch_retry(_url)
+      }); 
+    }
+    return fetch_retry(url);
+  });
+}
+
+
+
+
+
+//Javacript for responsive navigation menu
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+//Javacript for video slider navigation
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function (manual) {
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    contents.forEach((content) => {
+        content.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
+
+
+//drag & drop
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    document.getElementById("div1").style.color = "black";
+
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    document.getElementById("div1").style.color = "rgba(240, 128, 128, 0)"
+    document.getElementById("iframe").style.display = "block"
+}
+
+window.addEventListener("DOMContentLoaded", event => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.2;
+    audio.play();
+});
+
+window.addEventListener("click", () => {
+    document.getElementById("audio").play();
+});
+
+
+jQuery(document).ready(function($) {
+    var alterClass = function() {
+      var ww = document.body.clientWidth;
+      if (ww < 600) {
+        $('#hidden').removeClass('slider-navigation');
+      } else if (ww >= 601) {
+        $('#hidden').addClass('slider-navigation');
+      };
+    };
+    $(window).resize(function(){
+      alterClass();
+    });
+    //Fire it when the page first loads:
+    alterClass();
+  });
+
+
+
+
+
+ 
