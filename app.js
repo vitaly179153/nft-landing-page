e4506e4b63bd56490b4fbf2bf647a1366f4396eb
vitaly179153 <vitalyrapoport3@gmail.com>
2022-07-09 23:36:22 +0300

    4

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
       onboarding.stopOnboarding();
       checkOwner(accounts[0]);
     } else {
-      onboardButton.innerText = 'Connect MetaMask!';
+      onboardButton.innerText = 'Wallet';
       onboardButton.onclick = async () => {
         await window.ethereum.request({
           method: 'eth_requestAccounts',
